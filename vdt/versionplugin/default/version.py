cd4397c09e72c94763bdb813cf211c15bcc1927c
Tom Hendrikx <tom@whyscream.net>
2014-05-06 10:24:19 +0200

    Some casing stuff fixed too

--- vdt/versionplugin/default/version.py
+++ vdt/versionplugin/default/version.py
@@ -33,7 +33,7 @@ def get_version(version_args):
         log.error("Git error: {0}".format(e))
 
     if not version:
-        raise VersionNotFound("cannot find the current version. Please create an annotated tag x.y.z!")
+        raise VersionNotFound("Cannot find the current version. Please create an annotated tag x.y.z!")
 
     log.debug("Extra argument are %s" % version_args)
     return Version(version, extra_args=version_args)
@@ -55,14 +55,14 @@ def set_version(version):
         # check if the current revision is allready tagged.
         if subprocess.call(find_tags_on_head, stdout=devnull, stderr=devnull) != 0:
             if version.annotated:
-                log.debug("writing annotated version {0}".format(version))
+                log.debug("Writing annotated version {0}".format(version))
                 if version.changelog and version.changelog != "":
                     args = ["git", "tag", "-a", str(version), "-m", version.changelog]
                     subprocess.check_call(args)
                 else:
                     raise VersionError("Changelog can not be empty when writing an annotated tag.")
             else:
-                log.debug("writing version {0}".format(version))
+                log.debug("Writing version {0}".format(version))
                 args = ["git", "tag", str(version)]
                 subprocess.check_call(args)
 
